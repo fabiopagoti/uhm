e01b0c2460719410357ca5a5a4d4e36c05e777c5
sidneygabriel <sidneygabriel@gmail.com>
2015-08-20 14:34:43 -0300

    ainda testes

---
+++ quiz/view/Quiz.controller.js
@@ -0,0 +1,27 @@
+(function() {
+	"use strict";
+
+	sap.ui.controller("quiz.view.Quiz", {
+
+		
+		 onInit: function() {
+		 	
+  			
+		 },
+
+		
+     onQuizPress:function(){
+    	
+     },
+     
+     onCadPress: function() {
+		
+	},
+	
+	 onGraficPress: function() {
+		
+	}
+		
+		
+	});
+}());
