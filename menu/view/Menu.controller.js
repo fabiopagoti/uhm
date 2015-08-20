de619e7b415304be5b0b8f87c7e367bf82cfb092
sidneygabriel <sidneygabriel@gmail.com>
2015-08-20 17:50:41 -0300

    teste

--- menu/view/Menu.controller.js
+++ menu/view/Menu.controller.js
@@ -12,21 +12,7 @@
 		
      onQuizPress:function(){
     	 
-    	 var CController = Controller.extend("sap.m.sample.NavContainer.C", {
-
-    			handleNav: function(evt) {
-    				var navCon = this.getView().byId("navCon");
-    				var target = evt.getSource().data("target");
-    				if (target) {
-    					navCon.to(this.getView().byId(target));
-    				} else {
-    					navCon.back();
-    				}
-    			}
-    		});
-
-
-    		return CController;
+    	 
      },
      
      onCadPress: function() {
