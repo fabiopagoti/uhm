2f61f49156692dbb071aa00fce1038f6c92118b6
sidneygabriel <sidneygabriel@gmail.com>
2015-08-19 21:47:34 -0300

    Versao Sidney 1

---
+++ menu/view/Menu.controller.js
@@ -0,0 +1,19 @@
+(function() {
+	"use strict";
+
+	sap.ui.controller("menu.view.Menu", {
+
+		
+		 onInit: function() {
+		 	
+  			
+		 },
+
+		
+     onButtonPress:function(){
+    	 alert("teste");
+     }
+		
+		
+	});
+}());
