321a34de9d77a8b23d5046d48de9a54c3f74af4b
Fabio Pagoti <fabiopagoti@gmail.com>
2015-08-24 19:09:04 -0300

    Ask component

--- ask/view/Ask.controller.js
+++ ask/view/Ask.controller.js
@@ -1,14 +1,34 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
-    "use strict";
+	"sap/ui/core/mvc/Controller"
+	], function(Controller) {
+		"use strict";
 
-    return Controller.extend("ask.view.Ask", {
+		return Controller.extend("ask.view.Ask", {
 
-        onInit: function() {
+			onInit: function() {
 
-        }
+			},
 
-    });
+			onPressAskQuestion: function(){
+				jQuery.sap.log.debug("onPressAskQuestion");
+				var firebase = this.getOwnerComponent().firebase;
+				firebase.child("questions").push(this.getQuestionObject(),this.onCompleteQuestionAsked);
+			},
 
-});
+			getQuestionObject: function(){
+				jQuery.sap.log.debug("getQuestionObject");
+				return {
+					topic: [this.byId("topic").getSelectedKey()],
+					text: this.byId("question").getValue(),
+					type: this.byId("questionType").getSelectedKey(),
+					answer: ["A"]
+				};
+			},
+
+			onCompleteQuestionAsked: function(e){
+				jQuery.sap.log.debug("onCompleteQuestionAsked");
+			}
+
+		});
+
+	});
